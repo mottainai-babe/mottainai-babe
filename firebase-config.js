7f891cd90d0857b0844c5dd7a86eb0b43604abf7
mottainai-babe <ainazmum2624@gmail.com>
2026-02-24 23:22:08 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-// firebase-config.js
-// ✅ index.html / app.html 과 같은 폴더(루트)에 있어야 합니다.
-
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAEK6aDtbuxU9k2bunygBYgHhm75MeOlFU",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAEk6aDtbuXU9K2bUnygYBqHMh75MeOlFU",
   authDomain: "mottainai-babe.firebaseapp.com",
   projectId: "mottainai-babe",
   storageBucket: "mottainai-babe.firebasestorage.app",
   messagingSenderId: "752649559489",
-  appId: "1:752649559489:web:12888daedef7280eb7a340",
+  appId: "1:752649559489:web:1288d8adeef7280eb7a340",
   measurementId: "G-9HNDHB2G1Q"
 };
