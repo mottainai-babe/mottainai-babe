a816641b254b3149efa07b59401d628ac89ad816
mottainai-babe <ainazmum2624@gmail.com>
2026-02-24 22:44:41 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,11 @@
 // ✅ index.html / app.html 과 같은 폴더(루트)에 있어야 합니다.
 
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAEk6aDtbUXU9k2bUnygYBqHMh75Me0lFU",
+  apiKey: "AIzaSyAEK6aDtbuxU9k2bunygBYgHhm75MeOlFU",
   authDomain: "mottainai-babe.firebaseapp.com",
   projectId: "mottainai-babe",
   storageBucket: "mottainai-babe.firebasestorage.app",
   messagingSenderId: "752649559489",
-  appId: "1:752649559489:web:1288d8adeef7280eb7a340",
+  appId: "1:752649559489:web:12888daedef7280eb7a340",
   measurementId: "G-9HNDHB2G1Q"
 };
