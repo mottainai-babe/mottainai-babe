fc65ca1090b01de2c3334fb224d15dff13bdc033
mottainai-babe <ainazmum2624@gmail.com>
2026-02-24 14:57:13 +0900

    Create firebase-configs.js

---
+++ firebase-configs.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+// ✅ 이 파일은 index.html/app.html 과 같은 폴더(루트)에 있어야 합니다.
+
+// firebase-config.js
+// ✅ 이 파일은 index.html/app.html 과 같은 폴더(루트)에 있어야 합니다.
+
+// firebase-config.js
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAEk6aDtbuXU9K2bUnyYBqHMh75Me0lFU",
+  authDomain: "mottainai-babe.firebaseapp.com",
+  projectId: "mottainai-babe",
+  storageBucket: "mottainai-babe.firebasestorage.app",
+  messagingSenderId: "752649559489",
+  appId: "1:752649559489:web:1288d8adeef7280eb7a340",
+  measurementId: "G-9HNDHB2G1Q"
+};
